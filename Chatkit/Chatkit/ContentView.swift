190747dad482b446c4ff9569d12db1cfa06654d7
Tushar <tlimaye91@gmail.com>
2019-09-23 11:39:21 +0530

    Initial commit with vanilla iOS app

---
+++ Chatkit/Chatkit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chatkit
+//
+//  Created by Tushar Limaye on 23/09/19.
+//  Copyright © 2019 SwiftMumbai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
